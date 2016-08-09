af2a0147be16c7c862c41d8f2074573d9a3ca1c7
Diego <diego@Mate>
2016-08-09 10:47:44 -0300

    cambio

--- apps/alumnos/modules/ingreso/actions/actions.class.php
+++ apps/alumnos/modules/ingreso/actions/actions.class.php
@@ -14,22 +14,14 @@ class ingresoActions extends sfActions
   {
 	$user = $this->getUser();
 
+
+
 	if($user->isAuthenticated()) {
-	  	//obtengo datos del usuario
+		//obtengo datos del usuario
 		$this->usuario=$user->getUsername();
 		$this->idarea = $user->getProfile()->getIdarea();
 		$oAreas = Doctrine::getTable('Areas')->find(1);
-		if($this->getUser()->hasCredential('DesignacionesProfesores')) {
-			$this->area = "DesignacionesProfesores";
-		} elseif ($this->getUser()->hasCredential('DesignacionesSedes')) {
-			$this->area = "DesignacionesSedes";
-		} elseif ($this->getUser()->hasCredential('adminProfesores')) {
-			$this->area = "adminProfesores";	
-		} else {
-			$current_link = "http://$_SERVER[HTTP_HOST]";
-			$this->redirect($current_link."/profesores.php/logout");
-		}
-		$this->area = "DesignacionesSedes";
+		$this->area = "Alumno";
   	} else {
   		$this->usuario = "";
   	}
@@ -39,7 +31,7 @@ class ingresoActions extends sfActions
   {
   	//obtengo datos del usuario
 	$this->usuario=$this->getUser()->getUsername();
-	$this->idarea = $this->getUser()->getAttribute('id_area');
+	$this->idarea = 1;
 	$oAreas = Doctrine::getTable('Areas')->find($this->idarea);
 	$this->area= $oAreas->getDescripcion();
   }  
