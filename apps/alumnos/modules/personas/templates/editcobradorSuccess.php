dbef9285b9bb65bf5b347169e6d0eb581fb9ea9b
Diego <diego@Mate>
2016-08-16 11:37:43 -0300

    nuevo form

---
+++ apps/alumnos/modules/personas/templates/editcobradorSuccess.php
@@ -0,0 +1,9 @@
+ <style type="text/css">
+    p { margin-left:5em; /* Or another measurement unit, like px */ }
+  </style>
+
+<br>
+<h1>Editar Cobradores</h1>
+<br>
+
+<?php include_partial('formcobrador', array('form' => $form)) ?>
