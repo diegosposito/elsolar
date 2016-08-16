8cadd4bd35c0b2ef848b7963af9c68ba1b6db851
Diego <diego@Mate>
2016-08-16 11:37:58 -0300

    nuevo form

---
+++ apps/alumnos/modules/personas/templates/_formcobrador.php
@@ -0,0 +1,22 @@
+<?php use_stylesheets_for_form($form) ?>
+<?php use_javascripts_for_form($form) ?>
+
+<form action="<?php echo url_for('personas/'.($form->getObject()->isNew() ? 'createcobrador' : 'updatecobrador').(!$form->getObject()->isNew() ? '?idpersona='.$form->getObject()->getIdpersona() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
+<?php if (!$form->getObject()->isNew()): ?>
+<input type="hidden" name="sf_method" value="put" />
+<?php endif; ?>
+  <table>
+    <tfoot>
+      <tr>
+        <td colspan="2">
+          &nbsp;<a href="<?php echo url_for('personas/buscarcobrador') ?>">Volver al listado de Cobradores</a>
+          
+          <input type="submit" value="Save" />
+        </td>
+      </tr>
+    </tfoot>
+    <tbody>
+      <?php echo $form ?>
+    </tbody>
+  </table>
+</form>
