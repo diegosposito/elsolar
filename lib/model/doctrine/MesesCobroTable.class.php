7e618a25f851c343f3087f323df2be061cf57bc6
diegosposito <spositod@gmail.com>
2016-08-18 22:15:18 -0300

    nuevo metodo

--- lib/model/doctrine/MesesCobroTable.class.php
+++ lib/model/doctrine/MesesCobroTable.class.php
@@ -27,6 +27,23 @@ class MesesCobroTable extends Doctrine_Table
             return $q;
     }
 
+     // Alumnos cursando materia seleccionada y materias correlativas aprobadas
+    public static function obtenerSoloMesesCobro($idpersona)
+    {
+        $q = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAssoc("
+            select mes from meses_cobro where idpersona = ".$idpersona." 
+        ");
+
+        $arreglo = '';
+        
+        foreach($q as $mes) {
+            $arreglo[] = $mes['mes'];
+        }
+
+        return $arreglo;
+        
+    }
+
     public static function borrarMesesCobro($idpersona)
     {
            $q = Doctrine_Query::create()
