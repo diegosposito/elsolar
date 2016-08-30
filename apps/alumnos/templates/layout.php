95bcc5be8674432ed28451137965a7cf178c0dca
Diego <diego@Mate>
2016-08-30 11:52:23 -0300

    cambios

--- apps/alumnos/templates/layout.php
+++ apps/alumnos/templates/layout.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
 <head>
+    <title>CSS Free Templates with jQuery Slider</title>
     <?php include_http_metas() ?>
     <?php include_metas() ?>
     <?php include_title() ?>
@@ -15,6 +16,8 @@
     <?php use_stylesheet('superfish-vertical.css') ?>
     <?php use_stylesheet('superfish-navbar.css') ?>
     <?php use_stylesheet('ddaccordion.css') ?>
+    <?php use_stylesheet('style.css') ?>
+    <?php use_stylesheet('prettyCheckboxes.css') ?>
     
     <?php //use_javascript('webcam.js') ?>       
     <?php use_javascript('hoverIntent.js') ?>
@@ -25,65 +28,40 @@
     <?php use_javascript('tiny_mce/tiny_mce.js') ?>    
     <?php use_javascript('jquery.ui.timepicker.js') ?>
     <?php use_javascript('jquery.tablescroll.js') ?>
+    <?php use_javascript('jquery-1.7.min.js') ?>
+    <?php use_javascript('jquery.jcarousel.js') ?>
+    <?php use_javascript('DD_belatedPNG-min.js') ?>
+    <?php use_javascript('functions.js') ?>
     <?php use_javascript('ddaccordion.js') ?>
  
     <?php include_stylesheets() ?>
     <?php include_javascripts() ?>
-<script type="text/javascript">
-	// initialise plugins
-	jQuery(function(){
-		$('ul.sf-menu sf-navbar').superfish({
-			pathClass:  'current' 
-		});
-		
-		$('ul.sf-menu sf-vertical').superfish({ 
-    		animation: {height:'show'},   // slide-down effect without fade-in
-			delay:     1200               // 1.2 second delay on mouseout 
-		}); 
-	});
-</script> 
-
-<script type="text/javascript">
-ddaccordion.init({
-	headerclass: "submenuheader", //Shared CSS class name of headers group
-	contentclass: "submenu", //Shared CSS class name of contents group
-	revealtype: "click", //Reveal content when user clicks or onmouseover the header? Valid value: "click", "clickgo", or "mouseover"
-	mouseoverdelay: 200, //if revealtype="mouseover", set delay in milliseconds before header expands onMouseover
-	collapseprev: true, //Collapse previous content (so only one open at any time)? true/false 
-	defaultexpanded: [], //index of content(s) open by default [index1, index2, etc] [] denotes no content
-	onemustopen: false, //Specify whether at least one header should be open always (so never all headers closed)
-	animatedefault: false, //Should contents open by default be animated into view?
-	persiststate: true, //persist state of opened contents within browser session?
-	toggleclass: ["", ""], //Two CSS classes to be applied to the header when it's collapsed and expanded, respectively ["class1", "class2"]
-	togglehtml: ["suffix", "<img src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/plus.gif' class='statusicon' />", "<img src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/minus.gif' class='statusicon' />"], //Additional HTML added to the header when it's collapsed and expanded, respectively  ["position", "html1", "html2"] (see docs)
-	animatespeed: "fast", //speed of animation: integer in milliseconds (ie: 200), or keywords "fast", "normal", or "slow"
-	oninit:function(headers, expandedindices){ //custom code to run when headers have initalized
-		//do nothing
-	},
-	onopenclose:function(header, index, state, isuseractivated){ //custom code to run whenever a header is opened or closed
-		//do nothing
-	}
-})
-</script>
 </head>
 <body>
-<table align="center" bgcolor="#ffffff" border="0" width="500">
-  <tbody>
-    <tr>
-      <td>
-      <table border="0" width="100%" cellpadding="0" cellspacing="0">
-          <tr>
-            <td colspan="2">
-            <img alt="" src="<?php echo $sf_request->getRelativeUrlRoot();?>/images/alcec3.jpg">
-            </td>
-          </tr>
-          <tr height="21px" bgcolor="#707173">
-          	<?php $esalumno=false; ?>
-			<?php if ($sf_user->isAuthenticated()) { ?>
-            <td align="center" >
-			 	<div class="sf-menu-container">
-					<ul class="sf-menu sf-navbar">
-					<?php
+<?php
+	if ($sf_user->isAuthenticated()) { 
+	    $esalumno = false;
+	    $credencial = '';
+		$arrCredenciales = array();
+		foreach ($sf_user->getCredentials() as $credencial) {
+			array_push($arrCredenciales, $credencial); 
+		}
+		$sis=$sf_user->getGuardUser()->obtenerSistemas();
+		$sf_user->setAttribute('idsede',$sf_user->getProfile()->getIdsede());
+	}	
+?>
+
+	<div class="shell">
+		<!-- Header -->
+		<div id="header">
+			<!-- Search  -->			
+			<div id="search">
+				<div class="search-holder">
+					<form action="" method="post">					
+						<input type="text" class="field" value="Keywords" title="Keywords" />
+						<input type="submit" value="" class="submit-button" />						
+					</form>
+					<?php if ($sf_user->isAuthenticated()) { 
 						$esalumno = false;
 						$credencial = '';
 						$arrCredenciales = array();
@@ -92,95 +70,150 @@ ddaccordion.init({
 						}
 						$sis=$sf_user->getGuardUser()->obtenerSistemas();
 					?>
-					<?php foreach ($sis as $sistema) { ?>
-						<li>
-							<a href="<?php echo $url; ?>"><?php echo $sistema->getDescripcion(); ?></a>
-						</li>
-					<?php } ?>							
+					<font color="#000000"><b><?php echo $sf_user->getGuardUser()->getUsername().' (<a href="'.url_for('@sf_guard_signout').'">Salir</a>)'; ?></b><br>
+					<?php } ?>	
+					<div class="cl"></div>
+				</div>	
+			</div>
+			<!-- END Search -->						
+			<div class="cl"></div>
+			<!-- Logo -->
+			<h1 id="logo"><a title="Home" href="#">Mega Store</a></h1>
+			<!-- Top Navigation -->
+			<div id="top-navigation">	
+				<ul>
+					<li>0 items  $ 0,00</li>
+					<li><a class="start" title="My Account" href="#"><span></span>My Account</a></li>
+					<li><a class="cart" title="shopping cart" href="#"><span></span>shopping cart </a></li>
+					<li><a class="end" title="checkout" href="#">checkout<span></span></a></li>				
+				</ul>		
+			</div>				
+			<!-- END Top Navigation -->	
+			<div class="cl"></div>		
+		</div>
+		<!-- END Header -->
+		<!-- Navigation -->
+		<div id="navigation">
+			<ul>
+				<li><a title="Home" href="#">Home<span class="sep-right"></span></a></li>
+				<li>
+					<a title="Games" href="#"><span class="sep-left"></span>Gamer<span class="sep-right"></span></a>
+				</li>
+				<li><a title="Abstract" href="#"><span class="sep-left"></span>abstract<span class="sep-right"></span></a></li>
+				<li>
+					<a title="Retro" href="#"><span class="sep-left"></span>Retro<span class="sep-right"></span></a>
+					<div class="dd">
+						<ul>z
+							<li><a title="Drop down menu 1" href="#"><span class="sep-left"></span>Drop down menu 1</a></li>
+							<li>
+								<a title="Drop down menu 2" href="#"><span class="sep-left"></span>Drop down menu 2</a>
+								<div class="dd">
+									<ul>
+										<li><a title="Drop down menu 1" href="#"><span class="sep-left"></span><span class="dd-first"></span>Drop down menu 1</a></li>
+										<li><a title="Drop down menu 2" href="#"><span class="sep-left"></span>Drop down menu 2</a></li>
+										<li><a title="Drop down menu 3" href="#"><span class="sep-left"></span>Drop down menu 3</a></li>										
+									</ul>
+								</div>
+							</li>
+							<li><a title="Drop down menu 3" href="#"><span class="sep-left"></span>Drop down menu 3</a></li>							
+						</ul>
+					</div>
+				</li>
+				<li><a title="HI Tech" href="#"><span class="sep-left"></span>HI Tech<span class="sep-right"></span></a></li>
+				<li><a title="For Children" href="#"><span class="sep-left"></span>For Children<span class="sep-right"></span></a></li>
+				<li><a title="For Ladies" href="#"><span class="sep-left"></span>For Ladies<span class="sep-right"></span></a></li>
+			</ul>
+			<div class="cl"></div>
+		</div>
+		<!-- END Navigation -->
+		<!-- Main  -->
+		<div id="main">
+			<!-- Slider -->
+			<div id="slider-holder">				
+				
+			</div>
+			<!-- END Slider -->
+
+            <!-- Content -->
+			<div id="content">				
+				 <?php echo $sf_content; ?>
+			</div>
+			<!-- ЕND Content  -->
+			<!-- Sidebar -->
+			<div id="sidebar">
+				<div class="box">
+					<h2>Quick Links</h2>
+					<ul>
+					    <?php echo '<li>'.link_to('Gestión Personas', 'personas/new').'</li>' ; ?>
+						<?php echo '<li>'.link_to('Gestión Personas', 'personas/new').'</li>' ; ?>
+						<?php echo '<li>'.link_to('Gestión Personas', 'personas/new').'</li>' ; ?>
+						<?php echo '<li>'.link_to('Gestión Personas', 'personas/new').'</li>' ; ?>
+						<?php echo '<li>'.link_to('Gestión Personas', 'personas/new').'</li>' ; ?>
+						<?php echo '<li>'.link_to('Gestión Personas', 'personas/new').'</li>' ; ?>
+						<?php echo '<li>'.link_to('Salir', 'personas/new').'</li>' ; ?>
 					</ul>
 				</div>
-            </td>
-            <td align="right" >
-            	<font color="#ffffff"><b><?php echo $sf_user->getGuardUser()->getUsername().' (<a href="'.url_for('@sf_guard_signout').'">Salir</a>)'; ?></b><br>
-				<b>Perfil: Administrador</b>
-				<?php	
-				$credencial = '';
-				$arrCredenciales = array();
-			?></font>         
-			</td>            
-			<?php } else { ?>
-			<td colspan="2">&nbsp</td>	
-            <?php } ?>   
-          </tr>          
-      </table>
-	<?php if($esalumno){ ?>
-	  <table><a href="">INGRESAR DESDE AQUI</a></table>
-	<?php 
-		exit;
-	}; ?>
-      <table border="0" cellpadding="0" bgcolor="#ffffff" cellspacing="0" width="100%">
-        <tbody>
-          <tr>
-            <td width="190" valign="top" >
- 				<div class="glossymenu">               
-            		<?php $mensaje_usuario=''; ?>
-		            <?php if ($sf_user->isAuthenticated()) { 
-						$sf_user->setAttribute('idsede',$sf_user->getProfile()->getIdsede());
-						$q = Doctrine_Query::create()
-							->distinct(true)
-							->from('Menu m')
-							->Where('m.idsistema = 1')
-							->andWhereIn('m.credencial', $arrCredenciales)
-							->orderBy('m.idgrupomenu, m.orden');
-						          	
-						$opciones_menu = $q->execute();     
-		
-						$grupo_menu_anterior = '';
-						$grupo_menu = '';
-						foreach ($opciones_menu as $item) {
-							$grupo_menu = $item->getIdgrupomenu();
-							
-							if($grupo_menu <> $grupo_menu_anterior) {
-								if ($grupo_menu_anterior != '') {
-									echo '</ul></div>';
-								}
-								echo '<a class="menuitem submenuheader" href="" >'.$item->getGrupomenu()->getDescripcion().'</a><div class="submenu"><ul>';
-							}
-							echo '<li>'.link_to($item->getDescripcion(), $item->getModulo()).'</li>' ;						              
-							$grupo_menu_anterior = $grupo_menu;
-						}		
-						echo '</ul></div>';	
-            		} ?> 
-            	</div>
-      		</td>
-            <td valign="top" style="border-left:1px solid #fcdb1c;">
-            <div id="column2">
-				<?php 
-					echo $mensaje_usuario;
-					echo $sf_content;
-				?>
+			</div>	
+			<!-- END Sidebar -->
+			<div class="cl"></div>
+			<!-- Feartured Products -->
+			<div class="products featured">
+				<h2>Featured Products</h2>
+				<div class="products-slider">
+					<ul>
+						<li>
+							<a title="Details" href="#"><img src="/images/1.jpg" alt="Silver half transparent with blue lights computer case" /></a>
+							<p>Model Name</p>
+						</li>
+					</ul>
+				</div>
 			</div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      </td>
-    </tr>
-  </tbody>
-  <tfoot>
-  	<tr>
-      <td>
-			<table width="100%" border="0">
-				<tr>
-					<td>
-					<img alt="" src="<?php echo $sf_request->getRelativeUrlRoot();?>/images/alcec2.jpg">
-					</td>
-				</tr> 
-			</table>
-	  </td>
-      </tr>
-  </tfoot>
-</table>
-<p style="text-align: center;"><small>copyright © 2016 A.L.C.E.C. | dsposito@gmail.com</small><br>
-</p>
-</body></html>
+			<!-- END Featured Products -->			
+			<!-- Footer  -->
+			<div id="footer">
+				<div id="footer-top"></div>
+				<div id="footer-middle">
+					<div class="col styles">
+						<h3>Styles</h3>
+						<ul>
+							<li><a title="gamer" href="#"><span class="bullet"></span>gamer</a></li>
+							<li><a title="abstract" href="#"><span class="bullet"></span>abstract</a></li>
+							<li><a title="retro" href="#"><span class="bullet"></span>retro</a></li>
+							<li><a title="hi tech" href="#"><span class="bullet"></span>hi tech</a></li>
+							<li><a title="for children" href="#"><span class="bullet"></span>for children</a></li>
+							<li><a title="for ladies" href="#"><span class="bullet"></span>for ladies</a></li>							
+						</ul>
+					</div>
+					<div class="col info">
+						<h3>Information</h3>
+						<ul>
+							<li><a title="About MEGAStore" href="#"><span class="bullet"></span>About MEGAStore</a></li>
+							<li><a title="Privacy Policy" href="#"><span class="bullet"></span>Privacy Policy</a></li>
+							<li><a title="Terms &amp; Conditions" href="#"><span class="bullet"></span>Terms &amp; Conditions</a></li>
+							<li><a title="Contact Us" href="#"><span class="bullet"></span>Contact Us</a></li>
+							<li><a title="Site Map" href="#"><span class="bullet"></span>Site Map</a></li>												
+						</ul>
+					</div>
+					<div class="col newsletter">
+						<h3>Newsletter</h3>
+						<p>NO SPAM! Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+						<form action="" method="post">
+							<div class="field-holder"><input type="text" class="field" value="Enter Your Email" title="Enter Your Email" /></div>
+							<div class="cl"></div>
+							<input type="checkbox" name="check-box" value="" id="check-box" />
+							<label for="check-box">Lorem ipsum dolor sit amet, consectetur </label>
+							<input type="submit" value="submit" class="submit-button" />
+						</form>
+					</div>
+					<div class="cl"></div>
+				</div>
+				<div id="footer-bottom">
+					<p>&copy; MegaStore. Design by <a href="http://css-free-templates.com/">CSS-FREE-TEMPLATES.COM</a></p>
+				</div>
+			</div>
+			<!-- END Footer -->
+		</div>
+		<!-- END Main -->
+	</div>	
+</body>
+</html>
