c948637b2bc9de65cd7ac231d7c66af19126ce25
diegosposito <spositod@gmail.com>
2016-08-30 21:25:27 -0300

    cambio

--- plugins/sfDoctrineGuardPlugin/modules/sfGuardUser/actions/actions.class.php
+++ plugins/sfDoctrineGuardPlugin/modules/sfGuardUser/actions/actions.class.php
@@ -23,41 +23,8 @@ class sfGuardUserActions extends autoSfGuardUserActions
       try {
       	
         $sf_guard_user = $form->save();
-	// si 
-	if (!($sf_guard_user->getProfile()->getIdarea()>0)) {
-		$sf_guard_user->getProfile()->setIdarea($this->getUser()->getAttribute('id_area'));
-		$sf_guard_user->save();
-        }
 
-        // verifica que el alumno tenga dni en administracion central, de lo contrario envia mail informando
-	//conexion webservice administrativo
-	$soapclient = new nusoap_client("http://190.228.68.197:9090/administracion/webservices/personacuenta.php?wsdl");
-       
-	$soapclient->setCredentials("root", "sistemas2009");
 
-       //llamamos la función implementada en el server.php de la siguiente manera
-      $resultado = $soapclient->call('obtenerpersona',array( 'value'=> $sf_guard_user->getProfile()->getNrodoc()));
-       
-       $this->persona = unserialize(base64_decode($resultado));
-
-	if ($this->persona==NULL) {
-	// busco datos del alumno en sistem Alumnos para informar al sector administrativo
-
-		        //$soapclient = new nusoap_client("http://192.168.2.197:9999/sumar.php?wsdl");
-		        $soapclient1 = new nusoap_client("http://192.168.2.196/ucu/sitio/webservices/personas.php?wsdl");
-		
-		       //llamamos la función implementada en el server.php de la siguiente manera
-	            $resultado1 = $soapclient1->call('obtenerpersona',array( 'value'=> $sf_guard_user->getProfile()->getNrodoc()));
-	
-	           $this->persona = unserialize(base64_decode($resultado1));
-
-			
-
-	$mensaje= "El alumno ".$this->persona['apellido'].', '.$this->persona['nombre'].", solicita activacion de DNI Administrativo, por favor verificar en que cuenta se deberia asignar el mismo. DNI:".$sf_guard_user->getProfile()->getNrodoc();
-
-	mail(sfConfig::get('app_envio_mail_administracion'), 'Solicitud de alta DNI : '.$this->persona['apellido'].', '.$this->persona['nombre'].'-'.$sf_guard_user->getProfile()->getNrodoc(), $mensaje,"FROM: ingguillermoz@gmail.com\nX-Mailer: PHP");
-
-        }
       } catch (Doctrine_Validator_Exception $e) {
 
         $errorStack = $form->getObject()->getErrorStack();
