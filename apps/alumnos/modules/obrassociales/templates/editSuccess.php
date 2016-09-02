4a58b647264393717128c1aa5350597496f04558
diegosposito <spositod@gmail.com>
2016-09-01 22:05:57 -0300

    cabmios

--- apps/alumnos/modules/obrassociales/templates/editSuccess.php
+++ apps/alumnos/modules/obrassociales/templates/editSuccess.php
@@ -1,3 +1,3 @@
-<h1>Edit Obras sociales</h1>
+<h1>Editar Obras sociales</h1>
 
 <?php include_partial('form', array('form' => $form)) ?>
--- apps/alumnos/modules/obrassociales/templates/indexSuccess.php
+++ apps/alumnos/modules/obrassociales/templates/indexSuccess.php
@@ -1,36 +1,37 @@
-<h1>Obras socialess List</h1>
+<h1>Listado de Obras sociales</h1>
 
-<table>
-  <thead>
-    <tr>
-      <th>Idobrasocial</th>
-      <th>Denominacion</th>
-      <th>Abreviada</th>
-      <th>Estado</th>
-      <th>Fechaarancel</th>
-      <th>Fechaultimoperiodo</th>
-      <th>Created at</th>
-      <th>Updated at</th>
-      <th>Created by</th>
-      <th>Updated by</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($obras_socialess as $obras_sociales): ?>
-    <tr>
-      <td><a href="<?php echo url_for('obrassociales/show?idobrasocial='.$obras_sociales->getIdobrasocial()) ?>"><?php echo $obras_sociales->getIdobrasocial() ?></a></td>
-      <td><?php echo $obras_sociales->getDenominacion() ?></td>
-      <td><?php echo $obras_sociales->getAbreviada() ?></td>
-      <td><?php echo $obras_sociales->getEstado() ?></td>
-      <td><?php echo $obras_sociales->getFechaarancel() ?></td>
-      <td><?php echo $obras_sociales->getFechaultimoperiodo() ?></td>
-      <td><?php echo $obras_sociales->getCreatedAt() ?></td>
-      <td><?php echo $obras_sociales->getUpdatedAt() ?></td>
-      <td><?php echo $obras_sociales->getCreatedBy() ?></td>
-      <td><?php echo $obras_sociales->getUpdatedBy() ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+<table cellspacing="0" class="stats">
+    <thead>
+      <tr>
+        <td width="5%" align="center" class="hed">Obra Social</td>
+        <td width="50%" align="center" class="hed">Abreviada</td>
+        <td width="40%" align="center" class="hed">Estado</td>
+        <td width="10%" align="center" class="hed">Fecha Arancel</td>
+        <td width="10%" align="center" class="hed">Fecha Ult Periodo</td>
+        <td width="10%" align="center" class="hed">Edicion</td>
+      </tr>
+    </thead>
+    <tbody>
+      <?php $i=0; ?>
+      <?php foreach ($obras_socialess as $obras_sociales){ ?>
+      <tr class="fila_<?php echo $i%2 ; ?>">
+        <td width="5%" align="center"><?php echo $obras_sociales->getDenominacion() ?></td>
+        <td width="50%"><?php echo $obras_sociales->getAbreviada() ?></td>
+        <?php $estado = ($obras_sociales->getEstado()==1) ? 'Habilitada' : 'No Habilitada'; ?>
+        <td width="40%"><?php echo $estado ?></td>
+        <td width="10%"><?php echo $obras_sociales->getFechaarancel() ?></td>
+        <td width="10%"><?php echo $obras_sociales->getFechaultimoperiodo() ?></td>
+        <td align="center"><?php echo link_to("Editar", 'obrassociales/edit?idobrasocial='.$obras_sociales->getIdobrasocial() ,'class="mhead"'); ?></td>
+      </tr>
+      <?php $i++; ?>
+      <?php } ?>
 
-  <a href="<?php echo url_for('obrassociales/new') ?>">New</a>
+      <br>
+  
+  <br><br>
+    </tbody>
+  </table>
+
+
+  <a href="<?php echo url_for('obrassociales/new') ?>">Nueva Obra Social</a>
+
