e6992a66c196e7d36a563fc2068def079fdc752a
Diego <diego@Mate>
2016-09-05 11:56:54 -0300

    form nuevo

---
+++ lib/form/doctrine/AutoridadesForm.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Autoridades form.
+ *
+ * @package    sig
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class AutoridadesForm extends BaseAutoridadesForm
+{
+  public function configure()
+  {
+  	    unset( $this['created_at'], $this['updated_at'], $this['created_by'], $this['updated_by'] );
+
+  	$this->setValidators(array(
+      'idautoridad'      => new sfValidatorChoice(array('choices' => array($this->getObject()->get('idautoridad')), 'empty_value' => $this->getObject()->get('idautoridad'), 'required' => false)),
+      'nombre'           => new sfValidatorString(array('max_length' => 200, 'required' => true)),
+      'idcargoautoridad' => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('CargoAutoridades'), 'required' => true)),
+    ));
+     
+     
+
+      $this->validatorSchema->setOption('allow_extra_fields',true); 
+  }
+}
