2e743dd6a226c1801fbe3b6b73104eeb395c919a
Diego <diego@Mate>
2016-09-05 10:03:23 -0300

    cambios

---
+++ apps/alumnos/modules/informes/templates/obrassocialesSuccess.php
@@ -0,0 +1,37 @@
+ <style type="text/css">
+    p { margin-left:5em; /* Or another measurement unit, like px */ }
+  </style>
+  <br>
+<h1 align="center" style="color:black;">Listado de Obras Sociales</h1>
+
+ <a href="<?php echo url_for('informes/obrassocialespdf') ?>"><img src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/printer.png' align='center' size='20' /></a>
+<table cellspacing="0" class="stats">
+    <thead>
+      <tr>
+        <td width="40%" align="center" class="hed">Obra Social</td>
+        <td width="5%" align="center" class="hed">Abrev.</td>
+        <td width="15%" align="center" class="hed">Estado</td>
+        <td width="10%" align="center" class="hed">Fec.Arancel</td>
+        <td width="10%" align="center" class="hed">Fec.Ult.Per</td>
+        <td width="10%" align="center" class="hed">Documentos</td>
+      </tr>
+    </thead>
+    <tbody>
+      <?php $i=0; ?>
+      <?php foreach ($obras_socialess as $obras_sociales){ ?>
+      <tr class="fila_<?php echo $i%2 ; ?>">
+        <td width="40%" align="center"><?php echo $obras_sociales->getDenominacion() ?></td>
+        <td width="5%"><?php echo $obras_sociales->getAbreviada() ?></td>
+        <?php $estado = ($obras_sociales->getEstado()==1) ? 'Habilitada' : 'No Habilitada'; ?>
+        <td width="15%"><?php echo $estado ?></td>
+        <td width="10%"><?php echo $obras_sociales->getFechaarancel() ?></td>
+        <td width="10%"><?php echo $obras_sociales->getFechaultimoperiodo() ?></td>
+        <td align="center"><?php echo link_to("Visualizar", 'obrassociales/show?idobrasocial='.$obras_sociales->getIdobrasocial() ,'class="mhead"'); ?></td>
+      </tr>
+      <?php $i++; ?>
+      <?php } ?>
+
+      <br>
+  
+    </tbody>
+  </table>
