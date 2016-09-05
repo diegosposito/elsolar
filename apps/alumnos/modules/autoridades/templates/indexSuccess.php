8e5f4d8e4b1eb873386c3c9a8569cc79f83cfa96
Diego <diego@Mate>
2016-09-05 11:46:57 -0300

    cambios

--- apps/alumnos/modules/autoridades/templates/indexSuccess.php
+++ apps/alumnos/modules/autoridades/templates/indexSuccess.php
@@ -1,30 +1,25 @@
-<h1>Autoridadess List</h1>
+<h1>Listado de Autoridades</h1>
 
 <table>
   <thead>
     <tr>
-      <th>Idautoridad</th>
       <th>Nombre</th>
       <th>Idcargoautoridad</th>
-      <th>Created at</th>
-      <th>Updated at</th>
-      <th>Created by</th>
-      <th>Updated by</th>
+      <th>Fecha Alta</th>
+      <th>Fecha Ult. Modificación</th>
     </tr>
   </thead>
   <tbody>
     <?php foreach ($autoridadess as $autoridades): ?>
     <tr>
-      <td><a href="<?php echo url_for('autoridades/show?idautoridad='.$autoridades->getIdautoridad()) ?>"><?php echo $autoridades->getIdautoridad() ?></a></td>
-      <td><?php echo $autoridades->getNombre() ?></td>
+      <td><a href="<?php echo url_for('autoridades/show?idautoridad='.$autoridades->getIdautoridad()) ?>"><?php echo $autoridades->getNombre() ?></a></td>
       <td><?php echo $autoridades->getIdcargoautoridad() ?></td>
       <td><?php echo $autoridades->getCreatedAt() ?></td>
       <td><?php echo $autoridades->getUpdatedAt() ?></td>
-      <td><?php echo $autoridades->getCreatedBy() ?></td>
-      <td><?php echo $autoridades->getUpdatedBy() ?></td>
+     
     </tr>
     <?php endforeach; ?>
   </tbody>
 </table>
 
-  <a href="<?php echo url_for('autoridades/new') ?>">New</a>
+  <a href="<?php echo url_for('autoridades/new') ?>">Nueva Autoridad</a>
