a587422df85fc4e56b64ed075bc0c33c0cc15fdb
diegosposito <spositod@gmail.com>
2016-09-05 18:14:40 -0300

    cambio

--- lib/form/doctrine/AutoridadesForm.class.php
+++ lib/form/doctrine/AutoridadesForm.class.php
@@ -12,13 +12,16 @@ class AutoridadesForm extends BaseAutoridadesForm
 {
   public function configure()
   {
-  	    unset( $this['created_at'], $this['updated_at'], $this['created_by'], $this['updated_by'] );
+  	 unset( $this['created_at'], $this['updated_at'], $this['created_by'], $this['updated_by'] );
 
   	$this->setValidators(array(
       'idautoridad'      => new sfValidatorChoice(array('choices' => array($this->getObject()->get('idautoridad')), 'empty_value' => $this->getObject()->get('idautoridad'), 'required' => false)),
       'nombre'           => new sfValidatorString(array('max_length' => 200, 'required' => true)),
       'idcargoautoridad' => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('CargoAutoridades'), 'required' => true)),
     ));
+
+    $this->widgetSchema->setLabel('idcargoautoridad', '<p align="left">Cargo de Autoridad:</p>');
+
      
      
 
