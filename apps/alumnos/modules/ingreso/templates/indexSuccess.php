dcb6c52f5f84c495f63c7d314a92f0c1d05f96fa
diegosposito <spositod@gmail.com>
2016-09-06 18:18:36 -0300

    cabmios

--- apps/alumnos/modules/ingreso/templates/indexSuccess.php
+++ apps/alumnos/modules/ingreso/templates/indexSuccess.php
@@ -1,11 +1,11 @@
 <div class="post">
-					<h2>Welcome to MEGAStore</h2>
+					<h2>HOME PAGE del Circulo Odontológico CdelU</h2>
 					<p><strong>Proin hendrerit vehicula cursus.</strong> In tempor metus eget magna varius sagittis et ac nisi. Vestibulum massa nisl, tincidunt at sodales a, adipiscing iaculis nunc. Sed rutrum justo vitae sem aliquet bibendum. <strong>Quisque vestibulum, velit ut cursus blandit, orci quam adipiscing arcu, a ultricies felis urna sed risus. Etiam in dolor eu massa sodales faucibus non et purus.</strong> Nam facilisis aliquet adipiscing. Maecenas ligula mi, porttitor faucibus imperdiet eu, porta nec urna. In vel ligula quam. </p>
 					<p>Proin in nunc nibh, in suscipit enim. Nunc fermentum metus ut diam faucibus rhoncus. Duis augue sapien, pharetra sit amet mollis at, luctus ut eros.</p>
 </div>
 
 <div class="post">
-					<h2>Welcome to MEGAStore</h2>
+					<h2>Bienvenido al nuevo sitio WEB Online</h2>
 					<img src="/images/1.jpg" alt="Silver half transparent with blue lights computer case" />
 					<p><strong>Proin hendrerit vehicula cursus.</strong> In tempor metus eget magna varius sagittis et ac nisi. Vestibulum massa nisl, tincidunt at sodales a, adipiscing iaculis nunc. Sed rutrum justo vitae sem aliquet bibendum. <strong>Quisque vestibulum, velit ut cursus blandit, orci quam adipiscing arcu, a ultricies felis urna sed risus. Etiam in dolor eu massa sodales faucibus non et purus.</strong> Nam facilisis aliquet adipiscing. Maecenas ligula mi, porttitor faucibus imperdiet eu, porta nec urna. In vel ligula quam. </p>
 					<p>Proin in nunc nibh, in suscipit enim. Nunc fermentum metus ut diam faucibus rhoncus. Duis augue sapien, pharetra sit amet mollis at, luctus ut eros.</p>
