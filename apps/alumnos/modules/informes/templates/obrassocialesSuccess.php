5edee3488a73991b808517b85f5b24c774dd778f
diegosposito <spositod@gmail.com>
2016-09-13 16:14:21 -0300

    cambios

--- apps/alumnos/modules/informes/templates/obrassocialesSuccess.php
+++ apps/alumnos/modules/informes/templates/obrassocialesSuccess.php
@@ -8,25 +8,25 @@
 <table cellspacing="0" class="stats">
     <thead>
       <tr>
-        <td width="33%" align="center" class="hed">Obra Social</td>
-        <td width="5%" align="center" class="hed">Abrev.</td>
-        <td width="12%" align="center" class="hed">General</td>
-        <td width="12%" align="center" class="hed">Prótesis</td>
+        <td width="32%" align="center" class="hed">Obra Social</td>
+        <td width="4%" align="center" class="hed">Abrev.</td>
+        <td width="13%" align="center" class="hed">General</td>
+        <td width="13%" align="center" class="hed">Prótesis</td>
         <td width="14%" align="center" class="hed">Ortodoncia</td>
-        <td width="12%" align="center" class="hed">Implantes</td>
-        <td width="12%" align="center" class="hed">Archivos</td>
+        <td width="13%" align="center" class="hed">Implantes</td>
+        <td width="11%" align="center" class="hed">Archivos</td>
       </tr>
     </thead>
     <tbody>
       <?php $i=0; ?>
       <?php foreach ($obras_socialess as $obras_sociales){ ?>
       <tr class="fila_<?php echo $i%2 ; ?>">
-        <td width="33%" align="center"><?php echo $obras_sociales->getDenominacion() ?></td>
-        <td width="5%"><?php echo $obras_sociales->getAbreviada() ?></td>
-        <td width="12%"><?php echo $obras_sociales->getGeneral() ? 'Habilitada' : 'No Habilitada'; ?></td>
-        <td width="12%"><?php echo $obras_sociales->getProtesis() ? 'Habilitada' : 'No Habilitada'; ?></td>
+        <td width="32%" align="center"><?php echo $obras_sociales->getDenominacion() ?></td>
+        <td width="4%"><?php echo $obras_sociales->getAbreviada() ?></td>
+        <td width="13%"><?php echo $obras_sociales->getGeneral() ? 'Habilitada' : 'No Habilitada'; ?></td>
+        <td width="13%"><?php echo $obras_sociales->getProtesis() ? 'Habilitada' : 'No Habilitada'; ?></td>
         <td width="14%"><?php echo $obras_sociales->getOrtodoncia() ? 'Habilitada' : 'No Habilitada'; ?></td>
-         <td width="12%"><?php echo $obras_sociales->getImplantes() ? 'Habilitada' : 'No Habilitada'; ?></td>
+         <td width="13%"><?php echo $obras_sociales->getImplantes() ? 'Habilitada' : 'No Habilitada'; ?></td>
         <td align="center"><?php echo link_to("Visualizar", 'informes/mostrararchivos?idobrasocial='.$obras_sociales->getIdobrasocial() ,'class="mhead"'); ?></td>
       </tr>
       <?php $i++; ?>
