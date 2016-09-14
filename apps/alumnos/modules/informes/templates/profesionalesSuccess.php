63e139faf910b699db262c44cd0f0bdb65294867
Diego <diego@Mate>
2016-09-14 09:15:53 -0300

    templates

--- apps/alumnos/modules/informes/templates/profesionalesSuccess.php
+++ apps/alumnos/modules/informes/templates/profesionalesSuccess.php
@@ -2,7 +2,7 @@
     p { margin-left:5em; /* Or another measurement unit, like px */ }
   </style>
   <br>
-<h1 align="center" style="color:black;">Listado de Profesionales</h1>
+<h1 align="center" style="color:black;">Profesionales Asociados</h1>
 
  <a href="<?php echo url_for('informes/profesionalespdf') ?>"><img src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/printer.png' align='center' size='20' /></a>
 <table cellspacing="0" class="stats">
