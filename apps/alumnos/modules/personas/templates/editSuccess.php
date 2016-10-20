0cb1b70144746f57d59ffc1d637663e1724dae9b
diego <spositod@gmail.com>
2016-10-20 14:43:12 -0300

    darle formato al form

--- apps/alumnos/modules/personas/templates/editSuccess.php
+++ apps/alumnos/modules/personas/templates/editSuccess.php
@@ -1,9 +1,15 @@
- <style type="text/css">
-    p { margin-left:5em; /* Or another measurement unit, like px */ }
-  </style>
+<style>
+table {
+    border-spacing: 0.5rem;
+    border-collapse: separate;
+    border-spacing: 8px;
+    border-color: gray;
+    margin: 0px auto;
 
+}
+</style>
 <br>
-<h1 align="center" style="color:black;">Editar Personal</h1>
+<p style="align:center;color:#000000;font-weight: bold;"><font size="5px">Personal</font><font size="3px"> &nbsp;->&nbsp;&nbsp;&nbsp;&nbsp;Editar información</font></p> 
 <br>
-
+<a href="<?php echo url_for('personas/buscar') ?>">Salir sin guardar</a>
 <?php include_partial('form', array('form' => $form)) ?>
