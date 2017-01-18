86526fa0fcaf373125f8661add83fb959113e9ea
diego <spositod@gmail.com>
2017-01-18 13:03:52 -0300

    cambiosw

--- lib/form/doctrine/ObrasSocialesForm.class.php
+++ lib/form/doctrine/ObrasSocialesForm.class.php
@@ -18,9 +18,10 @@ class ObrasSocialesForm extends BaseObrasSocialesForm
       // Se define los labels
 	    $this->widgetSchema->setLabel('denominacion', '<p align="left">Obra Social:</p>');
  	    $this->widgetSchema->setLabel('abreviada', '<p align="left">Nombre Abreviado:</p>');
-      $this->widgetSchema->setLabel('fechaaranceltexto', '<p align="left">Aranceles / Texto:</p>');
- 	    $this->widgetSchema->setLabel('fechaultimoperiodotexto', '<p align="left">Ultimo Período / Texto:</p>');
+      $this->widgetSchema->setLabel('fechaultimoperiodotexto', '<p align="left">Ultimo Período / Texto:</p>');
      /* $this->widgetSchema->setLabel('ninterno', '<p align="left">Nro.Interno:</p>');
+       $this->widgetSchema->setLabel('fechaaranceltexto', '<p align="left">Aranceles / Texto:</p>');
+     
       $this->widgetSchema->setLabel('general', '<p align="left">General:</p>');
       $this->widgetSchema->setLabel('protesis', '<p align="left">Prótesis:</p>');
       $this->widgetSchema->setLabel('ortodoncia', '<p align="left">Ortodoncia:</p>');
@@ -29,14 +30,14 @@ class ObrasSocialesForm extends BaseObrasSocialesForm
 
 
  	    
-      $this->widgetSchema['fechaarancel'] = new sfWidgetFormJQueryDate(array(
+   /*   $this->widgetSchema['fechaarancel'] = new sfWidgetFormJQueryDate(array(
     'config' => '{}',
     'image'=> sfContext::getInstance()->getRequest()->getRelativeUrlRoot().'/images/calendar.gif',
     'culture' => substr(sfContext::getInstance()->getUser()->getCulture(), 0, 2),
     'config' => "{firstDay: 1, dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'], 
       monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']}", 
             'date_widget' => new sfWidgetFormDate(array('format' => '%day%/%month%/%year%')) 
-        ));
+        )); */
 
         $this->widgetSchema['fechaultimoperiodo'] = new sfWidgetFormJQueryDate(array(
     'config' => '{}',
@@ -47,7 +48,6 @@ class ObrasSocialesForm extends BaseObrasSocialesForm
             'date_widget' => new sfWidgetFormDate(array('format' => '%day%/%month%/%year%')) 
         ));
 
-      $this->widgetSchema->setLabel('fechaarancel', '<p align="left">Fecha Arancel:</p>');
       $this->widgetSchema->setLabel('fechaultimoperiodo', '<p align="left">Ultimo período abonado:</p>');
 
       $arregloEstados = array('1' => 'Habilitada', '0' => 'No Habilitada');
@@ -59,9 +59,7 @@ class ObrasSocialesForm extends BaseObrasSocialesForm
         'denominacion' => new sfValidatorString(array('required' => true), array('required' => 'El nombre de la O.Social es obligatorio.')),
         'abreviada' => new sfValidatorString(array('max_length' => 200, 'required' => false)),
         'estado'    => new sfValidatorInteger(array('required' => false)),
-        'fechaarancel' => new sfValidatorString(array('max_length' => 200, 'required' => false)),
         'fechaultimoperiodo' => new sfValidatorString(array('max_length' => 200, 'required' => false)),
-        'fechaaranceltexto' => new sfValidatorString(array('max_length' => 200, 'required' => false)),
         'fechaultimoperiodotexto' => new sfValidatorString(array('max_length' => 200, 'required' => false)),
         ));
 
