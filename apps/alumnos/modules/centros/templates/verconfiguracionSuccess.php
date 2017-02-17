ca0c8736c131fda3234498290b76ff01f29605e5
diego <spositod@gmail.com>
2017-02-17 13:43:57 -0300

    cabmios

---
+++ apps/alumnos/modules/centros/templates/verconfiguracionSuccess.php
@@ -0,0 +1,36 @@
+  <style type="text/css">
+    p { margin-left:5em; /* Or another measurement unit, like px */ }
+  </style>
+  <br>
+<h1 align="center" style="color:black;">Configuraciones Generales</h1>
+
+<table align="center" width="80%" cellspacing="0" class="stats">
+    <thead>
+      <tr>
+        <td width="100%" align="center" class="hed">Configuraciones Generales del Sistema</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="fila_<?php echo 0 ; ?>">
+        <td width="100%"><img src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/new.png' align='center' size='20' />
+<a href="<?php echo url_for('centros/index') ?>">Servicios ofrecidos por la Institución</a></td>
+      </tr>
+      <tr class="fila_<?php echo 1 ; ?>">
+        <td width="100%"><img src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/new.png' align='center' size='20' />
+<a href="<?php echo url_for('listahorarios/index') ?>">Lista de horarios de Pacientes</a></td>
+      </tr>
+       <tr class="fila_<?php echo 1 ; ?>">
+        <td width="100%"><img src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/new.png' align='center' size='20' />
+<a href="<?php echo url_for('listahorarios/index') ?>">Lista de horarios de Pacientes</a></td>
+      </tr>
+      
+
+      <br>
+  
+    </tbody>
+  </table>
+
+
+ 
+
+
