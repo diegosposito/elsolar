77d4f3c1c281a817599a0bccbcb01846fe6a73ca
diego <spositod@gmail.com>
2017-03-16 14:54:57 -0300

    centros

--- apps/alumnos/modules/centros/templates/verconfiguracionSuccess.php
+++ apps/alumnos/modules/centros/templates/verconfiguracionSuccess.php
@@ -4,6 +4,7 @@
   <br>
 <h1 align="center" style="color:black;">Configuraciones Generales</h1>
 
+<!-- CONFIGURACIONES GENERALES -->
 <table align="center" width="80%" cellspacing="0" class="stats">
     <thead>
       <tr>
@@ -13,20 +14,42 @@
     <tbody>
       <tr class="fila_<?php echo 0 ; ?>">
         <td width="100%"><img src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/new.png' align='center' size='20' />
-<a href="<?php echo url_for('centros/index') ?>">Modalidades ofrecidas por la Institución</a></td>
+          <a href="<?php echo url_for('centros/index') ?>">Modalidades ofrecidas por la Institución</a>
+        </td>
       </tr>
       <tr class="fila_<?php echo 1 ; ?>">
         <td width="100%"><img src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/new.png' align='center' size='20' />
-<a href="<?php echo url_for('listahorarios/index') ?>">Lista de horarios de Pacientes</a></td>
+          <a href="<?php echo url_for('listahorarios/index') ?>">Lista de horarios de Pacientes</a>
+        </td>
       </tr>
-       <tr class="fila_<?php echo 1 ; ?>">
+      <tr class="fila_<?php echo 0 ; ?>">
         <td width="100%"><img src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/new.png' align='center' size='20' />
-<a href="<?php echo url_for('listahorarios/index') ?>">Lista de horarios de Pacientes</a></td>
+          <a href="<?php echo url_for('listahorarios/index') ?>">Lista de horarios de Pacientes</a>
+        </td>
       </tr>
-      
-
       <br>
   
+    </tbody>
+  </table>
+
+<!-- CONFIGURACIONES ARCHIVOS Y DOCUMENTOS ESTATICOS -->
+  <table align="center" width="80%" cellspacing="0" class="stats">
+    <thead>
+      <tr>
+        <td width="100%" align="center" class="hed">Configuración de Areas y Documentos</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr class="fila_<?php echo 0 ; ?>">
+        <td width="100%"><img src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/new.png' align='center' size='20' />
+          <a href="<?php echo url_for('areadocumentos/index') ?>">Areas de Documentos</a>
+        </td>
+      </tr>
+      <tr class="fila_<?php echo 1 ; ?>">
+        <td width="100%"><img src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/new.png' align='center' size='20' />
+          <a href="<?php echo url_for('documentosinstitucion/index') ?>">Documentos por Area</a></td>
+      </tr>
+      <br>
     </tbody>
   </table>
 
