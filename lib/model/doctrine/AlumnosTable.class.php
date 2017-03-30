2f1393c9549b672c6448373238591a4087b9a26a
diego <spositod@gmail.com>
2017-03-30 12:13:57 -0300

    cambios en logica de obtener personal

--- lib/model/doctrine/AlumnosTable.class.php
+++ lib/model/doctrine/AlumnosTable.class.php
@@ -344,7 +344,7 @@ class AlumnosTable extends Doctrine_Table
       
         if($tipocriterio==1) {
             $q = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAssoc("
-                SELECT sf.username, pe.* FROM personas pe JOIN sf_guard_user sf ON pe.idusuario = sf.id 
+                SELECT sf.username, pe.* FROM personas pe LEFT JOIN sf_guard_user sf ON pe.idusuario = sf.id 
                 WHERE
                         ((pe.apellido LIKE '%".$criterio."%') OR (pe.nombre LIKE '%".$criterio."%'))
                          AND pe.activo = ".$idactivo." 
@@ -352,7 +352,7 @@ class AlumnosTable extends Doctrine_Table
             ");
         }else{
             $q = Doctrine_Manager::getInstance()->getCurrentConnection()->fetchAssoc("
-                SELECT sf.username, pe.* FROM personas pe JOIN sf_guard_user sf ON pe.idusuario = sf.id 
+                SELECT sf.username, pe.* FROM personas pe LEFT JOIN sf_guard_user sf ON pe.idusuario = sf.id 
                     WHERE
                         (pe.nrodoc LIKE '%".$criterio."%') AND pe.activo = ".$idactivo." 
                          ORDER BY pe.apellido ASC, pe.nombre ASC
