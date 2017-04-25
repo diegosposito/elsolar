68db7437c2483a6fdd3b6d071c99d0c1ac344a77
DS <spositod@gmail.com>
2017-04-25 15:04:32 -0300

    agregar campos de madre y padre en ficha pacientes

--- apps/alumnos/modules/paciente/actions/actions.class.php
+++ apps/alumnos/modules/paciente/actions/actions.class.php
@@ -19,7 +19,7 @@ class pacienteActions extends sfActions
       ->execute();
 
     if ($request->isMethod(sfRequest::POST) && $request->getParameter('idbuscarname')<>''){
-      
+
       $this->pacientes = Doctrine_Core::getTable('Paciente')
       ->createQuery('a')
       ->where('a.apellido  like "%'.$request->getParameter('idbuscarname').'%"')
@@ -27,20 +27,20 @@ class pacienteActions extends sfActions
 
       $this->criterio = $request->getParameter('idbuscarname');
 
-    }  
+    }
 
   }
 
   public function executeGuardarinformacionpersonal(sfWebRequest $request) {
-      
+
       $numerodoc = $request->getParameter('nrodoc');
-      $nrodoc = preg_replace("/[^\d]/", "", $numerodoc);    
-    
+      $nrodoc = preg_replace("/[^\d]/", "", $numerodoc);
+
       $arr = explode('-', $request->getParameter('fechanac'));
       $fechanacimiento = $arr[2]."-".$arr[1]."-".$arr[0];
 
       $paciente = Doctrine::getTable('Paciente')->getPacienteIdByNroDoc($request->getParameter('nrodoc'));
-      
+
       if ($idpaciente<>''){
          $oPaciente = Doctrine::getTable('Paciente')->find($paciente->getId());
       } else {
@@ -50,14 +50,14 @@ class pacienteActions extends sfActions
         $oPaciente->setNrodoc($nrodoc);
       }
       // Guarda los datos personales
-      $oPaciente->setNrodoc($numerodoc);      
+      $oPaciente->setNrodoc($numerodoc);
       $oPaciente->setNombre(ucwords(strtolower($request->getParameter('nombre'))));
       $oPaciente->setApellido(strtoupper($request->getParameter('apellido')));
       $oPaciente->setIdsexo($request->getParameter('idsexo'));
       $oPaciente->setEstadocivil($request->getParameter('estadocivil'));
       $oPaciente->setIdciudadnac($request->getParameter('ciudadnacimiento'));
       $oPaciente->setFechanac($fechanacimiento);
-      $oPersona->save();    
+      $oPersona->save();
 
       echo json_encode(array("idpaciente"=>$oPaciente->getId(),"mensaje"=>"El Paciente ha sido guardado correctamente."));
 
@@ -96,7 +96,7 @@ class pacienteActions extends sfActions
     $this->forward404Unless($paciente = Doctrine_Core::getTable('Paciente')->find(array($request->getParameter('id'))), sprintf('Object paciente does not exist (%s).', $request->getParameter('id')));
     $this->form = new PacienteForm($paciente);
     $this->forward404Unless($this->paciente = Doctrine_Core::getTable('Paciente')->find(array($request->getParameter('id'))), sprintf('Object paciente does not exist (%s).', $request->getParameter('id')));
-    
+
   }
 
   public function executeUpdate(sfWebRequest $request)
@@ -126,16 +126,16 @@ class pacienteActions extends sfActions
     if ($form->isValid())
     {
 
-      $es_nuevo = true;$credencial_name= ''; $imagefile_name= ''; 
-      if ($request->getPostParameter('paciente[id]')>0) 
+      $es_nuevo = true;$credencial_name= ''; $imagefile_name= '';
+      if ($request->getPostParameter('paciente[id]')>0)
           $es_nuevo=false;
-      
+
       // Si es la edicion
       if (!$es_nuevo){
           $this->forward404Unless($paciente = Doctrine_Core::getTable('Paciente')->find(array($request->getPostParameter('paciente[id]'))), sprintf('Object paciente does not exist (%s).',$request->getPostParameter('paciente[id]')));
-          $credencial_name= $paciente->getCredencial(); 
+          $credencial_name= $paciente->getCredencial();
           $imagefile_name= $paciente->getImagefile();
-      } 
+      }
 
       $i=0; $recuperar_credencial = false; $recuperar_imagefile =false;
 
@@ -143,11 +143,11 @@ class pacienteActions extends sfActions
 
             if (strlen(trim($fileName['credencial']['name']))==0)
                 $recuperar_credencial = true;
-                 
-              
+
+
             if (strlen(trim($fileName['imagefile']['name']))==0)
                 $recuperar_imagefile = true;
-                      
+
       }
 
       $paciente = $form->save();
@@ -161,10 +161,11 @@ class pacienteActions extends sfActions
     // Graba cambios si corresponde de imagenes o recupera anteriores
       if (!$es_nuevo)
           $paciente->save();
-    
 
+
+    /* RECUPERAR ESTE CODIGO QUE VA
       $folder_path_name = sfConfig::get('app_pathfiles_folder')."/paciente/".$paciente->getId();
-      
+
       if (!is_dir($folder_path_name) && !mkdir($folder_path_name)){
           die("Error creando carpeta $uploaddir");
       }
@@ -176,16 +177,16 @@ class pacienteActions extends sfActions
               $targetFolder = sfConfig::get('app_pathfiles_folder')."/paciente/".$paciente->getId().'/'.$fileName['imagefile']['name'];
               move_uploaded_file($fileName['imagefile']['tmp_name'], $targetFolder);
               $paciente->setImagefile($fileName['imagefile']['name']);
-           }   
+           }
 
            if (trim($fileName['credencial']['name'])<>'') {
               $targetFolder = sfConfig::get('app_pathfiles_folder')."/paciente/".$paciente->getId().'/'.$fileName['credencial']['name'];
               move_uploaded_file($fileName['credencial']['tmp_name'], $targetFolder);
-              $paciente->setCredencial($fileName['credencial']['name']); 
-           }  
-     
-      } 
-    
+              $paciente->setCredencial($fileName['credencial']['name']);
+           }
+
+      } */
+
       $paciente->save();
 
       $this->redirect('paciente/edit?id='.$paciente->getId());
