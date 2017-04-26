d2d8166d683fa9ae33f2409f15febc7dd90a9abd
DS <spositod@gmail.com>
2017-04-26 14:58:51 -0300

    cabmios

--- apps/alumnos/modules/paciente/templates/_form.php
+++ apps/alumnos/modules/paciente/templates/_form.php
@@ -1,13 +1,13 @@
 <?php use_stylesheets_for_form($form) ?>
 <?php use_javascripts_for_form($form) ?>
 
-
+<a href="<?php echo url_for('paciente/index') ?>">Volver al listado</a>
 <form action="<?php echo url_for('paciente/'.($form->getObject()->isNew() ? 'create' : 'update').(!$form->getObject()->isNew() ? '?id='.$form->getObject()->getId() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
 <?php if (!$form->getObject()->isNew()): ?>
 <input type="hidden" name="sf_method" value="put" />
 <?php endif; ?>
 <div align="left">
-  <table style="width:400px" cellpadding="0" cellspacing="0"  class="stats" >
+  <table style="width:380px" cellpadding="0" cellspacing="0"  class="stats" >
     <tfoot>
       <tr align="left">
         <td colspan="6">
@@ -30,9 +30,7 @@
           <td>
             <?php echo $form['nombre']->render() ?>
           </td>
-          <td rowspan="5" width="10%">
-          </td>
-           <td rowspan="5" width="10%">
+         <td align="center" colspan="2" rowspan="5" width="10%">
             <?php if ($form->getObject()->isNew()){ ?>
             <img style="align:center; width: 180px; height: 180px;" align='center' size='20' />
             <?php } else { ?>
@@ -73,12 +71,10 @@
           </td>
         </tr>
        <tr>
-        <td colspan="1"><?php echo "<b>".$form['idsexo']->renderLabel()."</b>" ?></td>
-        <td colspan="3"><?php echo $form['idsexo'] ?></td>
-      </tr>
-       <tr>
-        <td colspan="1"><?php echo "<b>".$form['estadocivil']->renderLabel()."</b>" ?></td>
-        <td colspan="3"><?php echo $form['estadocivil'] ?></td>
+        <td><?php echo "<b>".$form['idsexo']->renderLabel()."</b>" ?></td>
+        <td><?php echo $form['idsexo'] ?></td>
+        <td><?php echo "<b>".$form['estadocivil']->renderLabel()."</b>" ?></td>
+        <td><?php echo $form['estadocivil'] ?></td>
       </tr>
       <tr>
         <td colspan="1"><?php echo "<b>".$form['direccion']->renderLabel()."</b>" ?></td>
@@ -92,6 +88,10 @@
         <td colspan="1"><?php echo "<b>".$form['idciudadnac']->renderLabel()."</b>" ?></td>
         <td colspan="3"><?php echo $form['idciudadnac'] ?></td>
       </tr>
+      <tr>
+       <td align="left"><?php echo "<b>".$form['imagefile']->renderLabel()."</b>" ?></td>
+       <td  colspan="3" align="left"><?php echo $form['imagefile'] ?></td>
+     </tr>
       <tr>
         <th align="center" colspan=4><?php echo ''.'D A T O S &nbsp;&nbsp;&nbsp;C O N T A C T O ' ?></th>
       </tr>
@@ -107,18 +107,11 @@
         <td align="left"><?php echo '' ?></td>
         <td align="left"><?php echo '' ?></td>
       </tr>
-
-      <tr>
-        <th align="center" colspan=4><?php echo '_' ?></th>
-      </tr>
       <tr>
         <th align="center" colspan=4><?php echo ''.'O B R A &nbsp;&nbsp;&nbsp;S O C I A L ' ?></th>
       </tr>
       <tr>
-        <td align="left" colspan=4><b><label id="osdescripcion" name="osdescripcion" for="osdescripcion">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label></b></td>
-      </tr>
-      <tr>
-          <td colspan="1" width="10%">
+          <td colspan="1">
             <?php echo "<b>".$form['idobrasocial']->renderLabel()."</b>" ?>
           </td>
           <td colspan="3">
@@ -126,53 +119,20 @@
           </td>
         </tr>
         <tr>
-          <td colspan="1" width="10%">
+          <td colspan="1">
             <?php echo "<b>".$form['nroafiliado']->renderLabel()."</b>" ?>
           </td>
           <td colspan="3">
             <?php echo $form['nroafiliado']->render() ?>
           </td>
         </tr>
-        <tr>
-          <td colspan="1" width="10%">
-            <?php echo "<b>".$form['titular']->renderLabel()."</b>" ?>
-          </td>
-          <td colspan="3">
-            <?php echo $form['titular']->render() ?>
-          </td>
-        </tr>
-        <tr>
-          <td colspan="1" width="10%">
-            <?php echo "<b>".$form['parentesco']->renderLabel()."</b>" ?>
-          </td>
-          <td colspan="3">
-            <?php echo $form['parentesco']->render() ?>
-          </td>
-        </tr>
-        <tr>
-        <td colspan="1"><?php echo "<b>".$form['anotaciones']->renderLabel()."</b>" ?></td>
-        <td colspan="3"><?php echo $form['anotaciones'] ?></td>
-      </tr>
-       <tr>
-        <td colspan="1"><?php echo "<b>".$form['historial']->renderLabel()."</b>" ?></td>
-        <td colspan="3"><?php echo $form['historial'] ?></td>
-      </tr>
 
-       <tr>
-        <td align="left"><?php echo "<b>".$form['imagefile']->renderLabel()."</b>" ?></td>
-        <td  colspan="3" align="left"><?php echo $form['imagefile'] ?></td>
-      </tr>
 
-      <tr>
-        <th align="center" colspan=4><?php echo '_' ?></th>
-      </tr>
       <tr>
         <th align="center" colspan=4><?php echo ''.'D A T O S &nbsp;&nbsp;&nbsp;M A D R E / A P O D E R A D O ' ?></th>
       </tr>
+
       <tr>
-        <td align="left" colspan=4><b><label id="osdescripcion" name="osdescripcion" for="osdescripcion">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label></b></td>
-      </tr>
-        <tr>
           <td colspan="1" width="10%">
             <?php echo "<b>".$form['mamnombre']->renderLabel()."</b>" ?>
           </td>
@@ -216,23 +176,18 @@
            <?php echo $form['mamdireccion']->render() ?>
          </td>
        </tr>
+
        <tr>
-         <th align="center" colspan=4><?php echo '_' ?></th>
+         <th align="center" colspan=4><?php echo ''.'D A T O S &nbsp;&nbsp;&nbsp;P A D R E / F A M I L I A R ' ?></th>
        </tr>
-        <tr>
-          <th align="center" colspan=4><?php echo ''.'D A T O S &nbsp;&nbsp;&nbsp;P A D R E / F A M I L I A R ' ?></th>
-        </tr>
-        <tr>
-          <td align="left" colspan=4><b><label id="osdescripcion" name="osdescripcion" for="osdescripcion">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label></b></td>
-        </tr>
-          <tr>
+       <tr>
             <td colspan="1" width="10%">
               <?php echo "<b>".$form['papnombre']->renderLabel()."</b>" ?>
             </td>
             <td colspan="3">
               <?php echo $form['papnombre']->render() ?>
             </td>
-          </tr>
+        </tr>
           <tr>
             <td width="10%">
               <?php echo "<b>".$form['papfechanac']->renderLabel()."</b>" ?>
